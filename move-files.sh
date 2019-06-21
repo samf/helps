ef9161988829e88adc81d39851d21e57c9daa6bb
Sam Falkner <sam.falkner@gmail.com>
2019-06-21 12:00:39 -0600

    newest version

--- move-files.sh
+++ move-files.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-
 #rsyncs the backup files to LaCie, an external hard drive opt file sent to opt_backups and
 #var file sent to var_backups
-
-rsync -avz /tmp_backup/db-backup-$(date "+%F")/var /Volumes/LaCie/var_backups
+{
+rsync -avz /tmp_backup/db-backup-$(date "+%F")/var --exclude atlassian/application-data/bamboo/xml-data /Volumes/LaCie/var_backups
 rsync -avz /tmp_backup/db-backup-$(date "+%F")/opt /Volumes/LaCie/opt_backups
-&> /dev/null
+}&> /dev/null
