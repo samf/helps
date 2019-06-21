b41c6ec5b7ecaaa0bd95defbd43b408012c3079c
Sam Falkner <sam.falkner@gmail.com>
2019-06-21 11:13:50 -0600

    add whitespace

--- backup-db.sh
+++ backup-db.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
-
 {
 #stop the satalite to prepair for backup
 /usr/sbin/rhn-satellite stop
-#switch user to oracle
-su - oracle -c'
-#makes 'd' a directery marked with the date for the backups excludes large xml data file
-d=db-backup-$(date "+%F") --exclude /var/atlassian/application-data/bamboo/xml-data
-#creates 'tmp_backup' directory containing 'd' as a child
-mkdir -p /tmp_backup/$d;
-#backs up to the directory excluding the xml-data (too large)
-db-control backup /tmp_backup/$d
-';
+#switch user to oracle and execute backup script
+su - oracle -c /Users/grantfalkner/bin/oracle-backup.sh
 #restart redhat satalite
 /usr/sbin/rhn-satellite start
 } &> /dev/null
