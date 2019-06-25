c209346bb02c420a274b4e6576e20e0859bc3652
Sam Falkner <sam.falkner@gmail.com>
2019-06-25 09:56:10 -0600

    whitespace

--- pgdb_backup.sh
+++ pgdb_backup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -x
+
 #call using comand "su - OWNER - c /tmp/grant/pgdb_backup.sh DATABASE_NAME"
+
 #t = title, d = date, 1 = database name argument
+
 d=$(date "+%F")
 t=/tmp_backup_$1
 mkdir -p $t/$d
 echo > $t/$d/${1}.bak
 pg_dump $1 > $t/$d/${1}.bak
-
