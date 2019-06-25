8fdc8b96b016d4f562dc44961ea50410fa8b6bc7
Sam Falkner <sam.falkner@gmail.com>
2019-06-25 09:58:31 -0600

    fix an extra space

--- move-files.sh
+++ move-files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#call using comand "su - OWNER - c /tmp/grant/move-files.sh DATABASE_NAME"
+#call using comand "su - OWNER -c /tmp/grant/move-files.sh DATABASE_NAME"
 d=$(date "+%F")
 t=/tmp_backup_$1
 p=$t/$d/${1}.bak
